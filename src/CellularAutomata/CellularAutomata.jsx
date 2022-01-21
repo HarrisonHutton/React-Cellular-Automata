e18fa63893f7fdf3f33571aaad074106a61a7c55
HarrisonHutton <harrison.hutton6@gmail.com>
2022-01-20 19:24:15 -0500

    Main grid is functional with mouse clicks

--- src/CellularAutomata/CellularAutomata.jsx
+++ src/CellularAutomata/CellularAutomata.jsx
@@ -1,22 +1,107 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Node from "./Node/Node";
 
 import "./CellularAutomata.css";
 
 export default class CellularAutomata extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: [],
+      mouseIsPressed: false,
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                Foo 
-                <Node>
+  componentDidMount() {
+    const grid = getInitialGrid();
+    this.setState({ grid });
+  }
 
-                </Node>
-            </div>
-        )
-    }
+  handleMouseDown(row, col) {
+    console.log(`Mouse down at ${row} ${col}`);
+    const newGrid = getNewGridWithAliveToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid, mouseIsPressed: true });
+  }
+
+  handleMouseEnter(row, col) {
+    if (!this.state.mouseIsPressed) return;
+    const newGrid = getNewGridWithAliveToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid });
+  }
+
+  handleMouseUp() {
+    this.setState({ mouseIsPressed: false });
+  }
+
+  render() {
+    const { grid } = this.state;
+
+    return (
+      <>
+        <button
+          onClick={() => {
+            this.setState({ grid: getInitialGrid() });
+          }}
+        >
+          Clear Board
+        </button>
+        <div className="grid">
+          {grid.map((row, rowIdx) => {
+            return (
+              <div key={rowIdx}>
+                {row.map((node, nodeIdx) => {
+                  const { row, col, isAlive } = node;
+                  return (
+                    <Node
+                      key={nodeIdx}
+                      row={row}
+                      col={col}
+                      isAlive={isAlive}
+                      mouseIsPressed={this.mouseIsPressed}
+                      onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                      onMouseEnter={(row, col) =>
+                        this.handleMouseEnter(row, col)
+                      }
+                      onMouseUp={() => this.handleMouseUp()}
+                    ></Node>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
+
+const getInitialGrid = () => {
+  const grid = [];
+  for (let row = 0; row < 30; row++) {
+    const currentRow = [];
+    for (let col = 0; col < 65; col++) {
+      currentRow.push(createNode(col, row));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+};
+
+const createNode = (col, row) => {
+  return {
+    col,
+    row,
+    isAlive: false,
+  };
+};
+
+const getNewGridWithAliveToggled = (grid, row, col) => {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isAlive: !node.isAlive,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
+};
--- src/CellularAutomata/Node/Node.jsx
+++ src/CellularAutomata/Node/Node.jsx
@@ -1,14 +1,22 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import "./Node.css";
 
 export default class Node extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  render() {
+    const { row, col, isAlive, onMouseDown, onMouseEnter, onMouseUp } =
+      this.props;
 
-    render() {
-        return <div className="node"></div>
-    }
+    const extraClassName = isAlive ? "node-alive" : "node-dead";
+
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}
+      ></div>
+    );
+  }
 }
